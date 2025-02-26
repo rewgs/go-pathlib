d0c58b004a89ceb9580a3f59864d6c608db2a99a
Alex Ruger <alexrugermusic@gmail.com>
2025-02-25 23:41:37 -0600

    Wrote purepath.Anchor() tests.

--- tests/purepath_test/purepath_test.go
+++ tests/purepath_test/purepath_test.go
@@ -7,13 +7,50 @@ import (
 	"github.com/rewgs/go-pathlib/tests/utils"
 )
 
+var testPath string = utils.GetTestsPath()
+
 // Simple test creating the Path struct. Does not affect I/O  beyond utils.MakeDir().
 func TestPurePath(t *testing.T) {
-	testsPath := utils.GetTestsPath()
-	utils.MakeDir(testsPath)
+	utils.MakeDir(testPath)
 
-	_, err := purepath.New(testsPath)
+	_, err := purepath.New(testPath)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestPurePosixPathAnchor(t *testing.T) {
+	utils.MakeDir(testPath)
+
+	path, err := purepath.New(testPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, ok := path.(purepath.PurePosixPath)
+	if !ok {
+		t.Skip()
+	}
+
+	if path.Anchor() != "/" {
+		t.Errorf("TestPurePosixPathAnchor(): Wanted: %s; got: %s\n", "/", path.Anchor())
+	}
+}
+
+func TestPureWindowsPathAnchor(t *testing.T) {
+	utils.MakeDir(testPath)
+
+	path, err := purepath.New(testPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, ok := path.(purepath.PureWindowsPath)
+	if !ok {
+		t.Skip()
+	}
+
+	if path.Anchor() == "" {
+		t.Error()
 	}
 }
