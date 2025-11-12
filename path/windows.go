72f15c537b3c2bf03b4bfb934f12c4de3afe48dc
Alex Ruger <alexrugermusic@gmail.com>
2025-11-12 15:19:20 -0800

    Began WindowsPath.IsJunction(); updated parity chart.

--- path/windows.go
+++ path/windows.go
@@ -11,3 +11,9 @@ func NewWindowsPath(pathsegments ...string) *WindowsPath {
 		newBase(pathsegments...),
 	}
 }
+
+// Return true if the path points to a junction, and false for any other type of file.
+// Currently only Windows supports junctions.
+func (p *WindowsPath) IsJunction() bool {
+	panic("WindowsPath.IsJunction() not yet implemented.")
+}
